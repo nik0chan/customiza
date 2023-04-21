24d40a220a9f1bee55f34281104bdc8ec8610fef
nik0chan <nik0chan@yoga.nik0chan.local>
2023-04-21 22:03:54 +0200

    correcciones

--- customiza.sh
+++ customiza.sh
@@ -1,22 +1,22 @@
 #!/bin/sh 
 
-# Instalando zsh + dependencias
+echo "Instalando zsh + dependencias"
 sudo apt-get -y install zsh curl git  
-zsh
-# Estableciendo zsh como shell por defecto
-
-# Descargando Oh my zsh
+echo "Descargando Oh my zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# Instalando tema powerlevel10k
+echo "Instalando tema powerlevel10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 #sed -i "s/^ZSH_THEME.*/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/g" $HOME/.zshrc
-# Configuramos el perfil de power10k
+echo" Configurrando el perfil de power10k"
 curl -o $HOME/.p10k.zsh https://raw.githubusercontent.com/nik0chan/customiza/p10k.zsh
 
-# Plugins zsh
-# Syntax Highlighting
+echo "Instalando plugins zsh"
+echo "Syntax Highlighting"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-# Autosuggestions
+echo " Autosuggestions"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+echo "Configurando zshrc"
+curl https://raw.githubusercontent.com/nik0chan/customiza/main/zshrc > $HOME/.zshrc
 
-curl -o $HOME/.zshrc https://raw.githubusercontent.com/nik0chan/customiza/zshrc
+echo "Estableciendo zsh como shell por defecto"
+chsh -s $(which zsh)
