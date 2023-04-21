6d86afa5bc24a45e46ea68c8bb8b0af00cef731e
nik0chan <nik0chan@yoga.nik0chan.local>
2023-04-21 22:41:20 +0200

    cambios

--- customiza.sh
+++ customiza.sh
@@ -7,8 +7,6 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 echo "Instalando tema powerlevel10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 #sed -i "s/^ZSH_THEME.*/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/g" $HOME/.zshrc
-echo" Configurrando el perfil de power10k"
-curl  https://raw.githubusercontent.com/nik0chan/customiza/p10k.zsh > $HOME/.p10k.zsh
 
 echo "Instalando plugins zsh"
 echo "Syntax Highlighting"
@@ -17,6 +15,7 @@ echo " Autosuggestions"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 echo "Configurando zshrc"
 curl https://raw.githubusercontent.com/nik0chan/customiza/main/zshrc > $HOME/.zshrc
-
+echo" Configurrando el perfil de power10k"
+curl https://raw.githubusercontent.com/nik0chan/customiza/main/p10k.zsh > $HOME/.p10k.zsh
 echo "Estableciendo zsh como shell por defecto"
 sudo chsh -s $(which zsh)
