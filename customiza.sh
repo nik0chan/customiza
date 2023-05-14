8a5bf209126eedaa72008a2357b22943831892fe
nik0chan <nik0chan@yoga.nik0chan.local>
2023-05-14 10:37:29 +0200

    modificacoines p10k y zsh adición fuentes Nerd

--- customiza.sh
+++ customiza.sh
@@ -1,13 +1,17 @@
 #!/bin/sh 
 
 echo "Instalando zsh + dependencias"
-sudo apt-get -y install zsh curl git  
+sudo apt-get -y install zsh curl git unzip 
 echo "Descargando Oh my zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "Instalando tema powerlevel10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 #sed -i "s/^ZSH_THEME.*/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/g" $HOME/.zshrc
-
+echo "Instalando Nerd fonts" 
+curl https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.0/CascadiaCode.zip -o /tmp/CascadiaCode.zip
+unzip -o -d /usr/share/fonts /tmp/CascadiaCode.zip
+rm -f /tmp/CascadiaCode.zip
+fc-cache -f -v
 echo "Instalando plugins zsh"
 echo "Syntax Highlighting"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
